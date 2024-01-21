009f94045e278b9e352cb3aad44389811b99347b
hassanbein <155234015+hassanbein@users.noreply.github.com>
2024-01-21 23:44:59 +0100

    Update suckless.sh l'OL étendu
    DF

--- suckless.sh
+++ suckless.sh
@@ -1,18 +1,25 @@
-sudo xbps-install -S && sudo xbps-install -u xbps && sudo xbps-install -Su
+
+sudo xbps-install -S 
+sudo xbps-install -u xbps 
+sudo xbps-install -Syu
 cd ~
-mkdir .config
+mkdir .config/
 cd .config/
-mkdir suckless && cd suckless/
-sudo xbps-install -Sy git base-devel vim tmux htop gcc make nerd-fonts noto-fonts-emoji noto-fonts-cjk papirus-folders papirus-icon-theme intel-media-driver 
-sudo xbps-install -Sy lsd mpv feh picom ffmpeg bat  tty-clock rsync terminus-font  firefox  bash-completion
+mkdir suckless/ 
+cd suckless/
+sudo xbps-install -Sy make vim tmux htop firefox nerd-fonts
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/dwm
 git clone https://git.suckless.org/st
 git clone https://git.suckless.org/slstatus
-sudo xbps-install -Sy xorg base-devel libX11-devel libXft-devel libXinerama-devel
-cd st/ && make && sudo make clean install
-cd ../dwm && make && sudo make clean install
+sudo xbps-install -Sy xorg base-devel libX11-devel libXft-devel libXinerama-devel gcc python feh libXrandr-devel ffmpeg mpv mpd alacritty zathura scrot vlc gimp 
+cd st/ 
+make
+sudo make clean install
+cd ../dwm 
+make ; sudo make clean install
 cd ../dmenu && make && sudo make clean install
 cd ../slstatus && make && sudo make clean install
 cd ~
-echo "dwm &" >> .xinitrc
+mkdir .xinitrc
+echo "dwm" > .xinitrc
