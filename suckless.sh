607da9dcd9790e2780d9f22873332b616d0e099c
hassanbein <155234015+hassanbein@users.noreply.github.com>
2024-02-06 03:26:55 +0100

    Update suckless.sh

--- suckless.sh
+++ suckless.sh
@@ -7,16 +7,16 @@ mkdir .config/
 cd .config/
 mkdir suckless/ 
 cd suckless/
-sudo xbps-install -Sy make vim tmux htop firefox nerd-fonts
+sudo xbps-install -Sy make vim tmux htop firefox nerd-fonts libsensors
 git clone https://git.suckless.org/dmenu
 git clone https://github.com/hassanbein/dwm6.4_patch
 git clone https://github.com/hassanbein/st.git
 git clone https://github.com/hassanbein/slstatus
-sudo xbps-install -Sy xorg base-devel libX11-devel libXft-devel libXinerama-devel gcc python feh libXrandr-devel ffmpeg mpv mpd alacritty zathura scrot vlc gimp 
+sudo xbps-install -Sy xorg base-devel libX11-devel libXft-devel libXinerama-devel gcc python feh libXrandr-devel ffmpeg mpv mpd alacritty zathura scrot 
 cd st/ 
 make
 sudo make clean install
-cd ../dwm 
+cd ../dwm6.4_patch
 make ; sudo make clean install
 cd ../dmenu && make && sudo make clean install
 cd ../slstatus && make && sudo make clean install
